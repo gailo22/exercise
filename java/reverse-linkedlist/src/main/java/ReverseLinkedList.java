f13e0edaf5234f266b59cf1ed6751d03f3037a74
Montree Bungnasang <montree.bun@truedigital.com>
2021-06-14 12:38:12 +0700

    add solution

--- java/reverse-linkedlist/src/main/java/ReverseLinkedList.java
+++ java/reverse-linkedlist/src/main/java/ReverseLinkedList.java
@@ -3,7 +3,17 @@ import java.util.Arrays;
 class ReverseLinkedList {
 
     Node<Integer> reverse(Node<Integer> head) {
-        return null;
+        Node<Integer> curr = head;
+        Node<Integer> prev = null, next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
   
 }
